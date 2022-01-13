3b1e34641e4a4c09ba1cfa6a1cd0a2ee0145311b
OrdainedRat <marcusjuniusbrutus213@gmail.com>
2022-01-12 21:47:08 -0600

    delete and get posts functional

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -15,7 +15,7 @@ router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
   Users.get()
     .then(users => {
-      res.status(200).json({users, message: 'hello'})
+      res.status(200).json(users)
     })
     .catch(err => next(err))
 });
@@ -26,10 +26,14 @@ router.get('/:id', validateUserId, (req, res) => {
 });
 
 router.post('/', validateUser, (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
-  console.log('working')
-  console.log(req.body)
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(err)
+    })
+  
 });
 
 router.put('/:id', validateUserId, (req, res) => {
@@ -41,11 +45,21 @@ router.put('/:id', validateUserId, (req, res) => {
 router.delete('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  Users.remove(req.params.id)
+    .then(() => {
+      res.status(200).json(req.user)
+    })
+    .catch(err => res.status(500).json(err))
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(err => res.status(500).json(err))
 });
 
 router.post('/:id/posts', validateUserId, (req, res) => {
